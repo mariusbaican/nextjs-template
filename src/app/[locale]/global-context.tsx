f8433404171c3cca6d0943e92279afb92ec790af
Marius Baican <marius.baican18@gmail.com>
2025-07-31 15:20:48 +0300

    Added globalContext breakpoint logic

--- src/app/[locale]/global-context.tsx
+++ src/app/[locale]/global-context.tsx
@@ -1,14 +1,48 @@
-// "use client";
-// import { createContext, ReactNode, useContext } from "react";
+"use client";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
-// type GlobalContextType = {};
+type Breakpoint = "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
 
-// const defaultValue: GlobalContextType = {};
+type GlobalContextType = {
+  currentBreakpoint: Breakpoint;
+};
 
-// const GlobalContext = createContext<GlobalContextType>(defaultValue);
+const defaultValue: GlobalContextType = {
+  currentBreakpoint: "2xl",
+};
 
-// export const GlobalProvider = ({ children }: { children: ReactNode }) => {
-//   return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
-// };
+const GlobalContext = createContext<GlobalContextType>(defaultValue);
 
-// export const useGlobalContext = () => useContext(GlobalContext);
+export const GlobalProvider = ({ children }: { children: ReactNode }) => {
+  const [currentBreakpoint, setCurrentBreakpoint] = useState<Breakpoint>("2xl");
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 640) setCurrentBreakpoint("xs");
+      else if (window.innerWidth < 768) setCurrentBreakpoint("sm");
+      else if (window.innerWidth < 1024) setCurrentBreakpoint("md");
+      else if (window.innerWidth < 1280) setCurrentBreakpoint("lg");
+      else if (window.innerWidth < 1536) setCurrentBreakpoint("xl");
+      else setCurrentBreakpoint("2xl");
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <GlobalContext.Provider value={{ currentBreakpoint }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(GlobalContext);
